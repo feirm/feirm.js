a278c30a75b7637693346eafedccb9f0b84a744c
Jack Coble <jack@coble.uk>
2022-01-15 21:24:16 +0000

    Add more types to interfaces

--- src/account/interfaces.ts
+++ src/account/interfaces.ts
@@ -11,7 +11,41 @@ interface EncryptedKey {
     salt: string;
 }
 
-export { 
+interface EncryptedAccount {
+    email?: string;
+    username: string;
+    identity_publickey: string;
+    encrypted_key: EncryptedKey;
+    created_at?: number;
+    version?: number;
+    totp?: TOTP;
+
+    token?: EphemeralToken;
+}
+
+// Representation of an ephemeral token signature payload
+interface EphemeralToken {
+    id?: string;
+    nonce?: string;
+    signature?: string;
+}
+
+// Representation of a signed session token
+interface SignedSessionToken {
+    token_id: string;
+    signature: string;
+}
+
+// Representation for TOTP field on sign-up
+interface TOTP {
+    secret?: string;
+    token?: string;
+}
+
+
+export {
     Keys,
-    EncryptedKey
+    EncryptedKey,
+    EncryptedAccount,
+    SignedSessionToken
 }
