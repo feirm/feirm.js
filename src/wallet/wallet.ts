0ebb3d74ae8276c7d68e263a749a5b2d4afb3afb
Jack Coble <jack@coble.uk>
2022-01-15 23:04:30 +0000

    Start of abstract class for wallets

---
+++ src/wallet/wallet.ts
@@ -0,0 +1,51 @@
+import bip39 from "bip39";
+
+abstract class Wallet {
+    private mnemonic: string; // BIP39 mnemonic
+
+    /**
+     * Wallet constructor
+     * @param mnemonic - BIP39 Mnemonic
+     */
+    constructor(mnemonic?: string) {
+        if (mnemonic && this.validMnemonic(mnemonic)) {
+            this.mnemonic = mnemonic;
+        }
+    }
+
+    /**
+     * Check if a mnemonic is BIP39 compliant
+     * @param mnemonic 
+     * @returns {boolean}
+     */
+    validMnemonic(mnemonic: string): boolean {
+        const valid = bip39.validateMnemonic(mnemonic);
+        return valid;
+    }
+
+    /**
+     * Set BIP39 Mnemonic in memory
+     * @param mnemonic - BIP39 Mnemonic
+     * @returns {void}
+     */
+    setMnemonic(mnemonic: string): Promise<void> {
+        if (!this.validMnemonic(mnemonic)) {
+            return Promise.reject("Mnemonic is invalid!");
+        }
+
+        this.mnemonic = mnemonic;
+        return Promise.resolve();
+    }
+
+    /**
+     * Securely generate a 24 word BIP39 mnemonic
+     * @returns {string}
+     */
+    generateMnemonic(): string {
+        // Generate some secure entropy and use it to create a 24 word mnemonic
+        const entropy = window.crypto.getRandomValues(new Uint8Array(32));
+        const mnemonic = bip39.entropyToMnemonic(Buffer.from(entropy));
+        
+        return mnemonic;
+    }
+}
