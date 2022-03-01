b6c629e74dd908b32901eed71cf14ed70d81ac59
Jack Coble <jack@coble.uk>
2022-03-01 19:30:45 +0000

    Export Bitcoin P2WPKH wallet class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Account } from "./account/account";
+import { BitcoinP2WPKH } from "./wallet/bitcoin-p2wpkh";
 import { Wallet } from "./wallet/wallet";
 
 export {
     Account,
-    Wallet
+    Wallet,
+    BitcoinP2WPKH
 }
