f9c992b7c5b4249c7f9ff0d8cc6f8f92da52c05c
Jack Coble <jack@coble.uk>
2022-03-13 09:31:29 +0000

    Export and update class name for Ethereum wallet

--- src/wallet/ethereum-bip44.ts
+++ src/wallet/ethereum-bip44.ts
@@ -2,8 +2,10 @@ import { Wallet } from "./wallet";
 /**
  * The Ethereum wallet class can be used for ETH-based networks, such as Ethereum or the Binance Smart Chain.
  */
-class Ethereum extends Wallet {
+class EthereumBIP44 extends Wallet {
     constructor() {
         super();
     }
 }
+
+export { EthereumBIP44 }
