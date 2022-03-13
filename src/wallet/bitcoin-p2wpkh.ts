ac55a4d1022a31ba4826943c3c15f0557f505a86
Jack Coble <jack@coble.uk>
2022-03-13 09:29:52 +0000

    Start of BIP44 Ethereum wallet

--- src/wallet/bitcoin-p2wpkh.ts
+++ src/wallet/bitcoin-p2wpkh.ts
@@ -3,6 +3,11 @@ import { Wallet } from "../wallet/wallet";
 class BitcoinP2WPKH extends Wallet {
     private xpub: string;
     private zpub: string;
+
+    constructor() {
+        super();
+        
+    }
 }
 
 export { BitcoinP2WPKH };
---
+++ src/wallet/ethereum-bip44.ts
@@ -0,0 +1,9 @@
+import { Wallet } from "./wallet";
+/**
+ * The Ethereum wallet class can be used for ETH-based networks, such as Ethereum or the Binance Smart Chain.
+ */
+class Ethereum extends Wallet {
+    constructor() {
+        super();
+    }
+}
